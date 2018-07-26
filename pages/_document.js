356eab1a569971cd75f11805dfb7d5ce4de56430
permanar <richie.permana@gmail.com>
2018-07-26 23:44:48 +0800

    Currently setting up for import css file only.

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
